59ab123134271ee72c54c886022c90a96437a094
Felipe <felipe@noduscompany.com>
2024-04-02 23:30:32 -0300

    Fix mobile version of Jokes

--- front/src/modules/jokes/views/Jokes.vue
+++ front/src/modules/jokes/views/Jokes.vue
@@ -1,18 +1,18 @@
 
 <template>
-  <div class="flex justify-end w-full pt-6 pr-6 dark:bg-primary bg-white ease-linear duration-100">
+  <div class="absolute sm:relative flex justify-end w-full pt-6 pr-6 dark:bg-primary bg-white ease-linear duration-100">
     <CustomButton @click="toggleJokeForm" type="primary" size="md" >
       Submit your own Joke
     </CustomButton>
-    <div class="w-1/2 z-20 h-72 rounded-lg bg-white border p-4 border-light dark:border-lighter absolute z-10 scale-0 opacity-0 ease-out duration-200 origin-top-right" :class="{'opacity-100 scale-100': jokeFormOpen}">
+    <div class="w-2/3 sm:w-1/2 z-20 h-72 rounded-lg bg-white border p-4 border-light dark:border-lighter absolute z-10 scale-0 opacity-0 ease-out duration-200 origin-top-right" :class="{'opacity-100 scale-100': jokeFormOpen}">
       <button @click="toggleJokeForm" class="flex items-center justify-center absolute z-10 top-4 right-4 rounded-full text-primary">
         <MaterialIcon icon="close" />
       </button>
       <JokeForm :submitcallback="toggleJokeForm" />
     </div>
   </div>
-  <div class="items-end sm:items-stretch min-h-fullscreen flex flex-col sm:flex-row sm:space-x-4 p-6 dark:bg-primary transition-colors ease-linear duration-100 relative">
-    <CustomButton @click="toggleFilters" type="secondary-full" circle class="sm:hidden sticky z-10 top-4 right-4">
+  <div class="items-start sm:items-stretch min-h-fullscreen flex flex-col sm:flex-row sm:space-x-4 p-6 dark:bg-primary transition-colors ease-linear duration-100 relative">
+    <CustomButton @click="toggleFilters" type="secondary-full" circle class="sm:hidden sticky z-10">
       <MaterialIcon icon="filter_list" />
     </CustomButton>
     <div class="absolute opacity-0 scale-0 z-10 bg-white dark:bg-primary w-72 ease-out duration-200 origin-top-right
